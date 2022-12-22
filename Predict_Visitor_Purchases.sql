4dbee33ee20f7805b51d91181f5f245c722d076d
TechieTeee <100870737+TechieTeee@users.noreply.github.com>
2022-12-22 03:11:11 -0600

    Update Predict_Visitor_Purchases.sql

--- Predict_Visitor_Purchases.sql
+++ Predict_Visitor_Purchases.sql
@@ -45,3 +45,29 @@ SELECT
   will_buy_on_return_visit
 FROM all_visitor_stats
 GROUP BY will_buy_on_return_visit
+
+                
+                                         
+                                         
+SELECT
+  * EXCEPT(fullVisitorId)
+FROM
+  # features
+  (SELECT
+    fullVisitorId,
+    IFNULL(totals.bounces, 0) AS bounces,
+    IFNULL(totals.timeOnSite, 0) AS time_on_site
+  FROM
+    `data-to-insights.ecommerce.web_analytics`
+  WHERE
+    totals.newVisits = 1)
+  JOIN
+  (SELECT
+    fullvisitorid,
+    IF(COUNTIF(totals.transactions > 0 AND totals.newVisits IS NULL) > 0, 1, 0) AS will_buy_on_return_visit
+  FROM
+      `data-to-insights.ecommerce.web_analytics`
+  GROUP BY fullvisitorid)
+  USING (fullVisitorId)
+ORDER BY time_on_site DESC
+LIMIT 10;
